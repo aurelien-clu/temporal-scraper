08d9f3c27ae553c3c20e0b3c1e543070ebea0159
Aurelien Somename <aurelien.somename@gmail.com>
2023-04-05 16:37:45 +0200

    wip

--- scraper_activities.py
+++ scraper_activities.py
@@ -7,16 +7,18 @@ from temporalio import activity
 async def fetch_page(url: str) -> str:
     logger.info(f"fetching, {url}!")
     async with httpx.AsyncClient() as client:
-        r = await client.get(url)
-    return "<html>Empty</html>"
+        response = await client.get(url)
+        response.raise_for_status()
+        content_str = response.text
+    return content_str
 
 
 @activity.defn
 async def parse_page(page: str) -> str:
-    logger.info(f"fetching, {page}!")
+    logger.info(f"parsing page.length={len(page)}!")
     return "Empty"
 
 
 @activity.defn
 async def save_page(parsed_page: str):
-    logger.info(f"saving, {parsed_page}!")
+    logger.info(f"saving parsed_page.length={len(parsed_page)}!")
