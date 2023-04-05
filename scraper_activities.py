c2cc45fdf2930af9c3dd9307620568611b53fa94
Aurelien Somename <aurelien.somename@gmail.com>
2023-04-05 21:11:18 +0200

    wip

--- scraper_activities.py
+++ scraper_activities.py
@@ -3,7 +3,8 @@ from loguru import logger
 from temporalio import activity
 
 from scraper_model import FetchedPage, ParsedPage, SavePage
-
+import bs4
+from urllib.parse import urlparse
 
 @activity.defn
 async def fetch_page(url: str) -> FetchedPage:
@@ -18,10 +19,23 @@ async def fetch_page(url: str) -> FetchedPage:
 @activity.defn
 async def parse_page(page: FetchedPage) -> ParsedPage:
     # TODO: find all links
-    # TODO: define dataclass inputs & outputs of each activity
     logger.debug(f"parsing!")
     title = ""
-    links = []
+
+    url_parsed = urlparse(page.url)
+
+    def extract_links(soup):
+        for node in soup.find_all("a"):
+            link = node.get('href')
+            if link.startswith("/"):
+                link = url_parsed.netloc + link
+            yield link
+
+    soup = bs4.BeautifulSoup(page.html_body, features="html.parser")
+    links = list(extract_links(soup))
+
+    for link in links:
+        logger.debug(link)
 
     return ParsedPage(url=page.url, title=title, links=links)
 
